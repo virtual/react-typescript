4ad195f7812831905bd1d395b9e8d8bb735810c4
Jeanine Schoessler <jeanine.mt@gmail.com>
2022-12-30 14:04:08 -0700

    add React component type for typescript props

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -3,6 +3,12 @@
 // should receive
 interface ChildProps {
   color: string;
+  
+  // Define a fx but don't expect a return value
+  onClick: () => void; 
+
+  // Required in React 18, note below
+  children?: React.ReactNode;
 }
 
 // There are 3-4 ways to implement props into the component
@@ -13,21 +19,47 @@ interface ChildProps {
 // };
 
 
-// 1. Pass props arg, but this might pull more than we need
+// 1a. Pass props arg, but this might pull more than we need
 // Typescript doesn't know we are defining a React component
 // React component default props (displayName) are invalid
 // export const Child = (props: ChildProps) => {
 //   return <div>Hey there</div>
 // };
 
-
-// 1. Pass props, but destructure props so we only pass
+// 1b. Pass props, but destructure props so we only pass
 // the props we want
-export const ChildAsProps = ({ color }: ChildProps) => {
-  return <div>Hey there, {color}</div>
+export const ChildAsProps = ({ color, onClick }: ChildProps) => {
+  return (
+    <div>Hey there, {color}
+    <button onClick={onClick}>Click</button>
+  </div>
+  );
 };
+
 
 // 2. Tells Typescript this is a React Component
-export const Child: React.FC<ChildProps> = ({ color }) => {
-  return <div>Hey there, {color}!</div>
+export const Child: React.FC<ChildProps> = ({ color, onClick }) => {
+  return (
+    <div>Hey there, {color}!
+    <button onClick={onClick}>Click</button>
+  </div>
+  );
 };
+
+// Allows you to access default properties with autocomplete
+// eg Child.displayName
+// Also can receive a children prop <Child>prop</Child> (React 17)
+
+/*
+React 18 introduces a breaking change with the removal 
+of implicit children in React.FunctionComponent types.
+
+To fix this, we now must include the children in the 
+interface along with our other props:
+
+interface ChildProps {
+  color: string;
+  onClick: () => void;
+  children?: React.ReactNode;
+}
+*/
--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -1,7 +1,12 @@
 import { Child } from './Child';
 
 const Parent = () => {
-  return <Child color="red" />
+  // return <Child color="red" onClick={() => { console.log('clicked')}} />
+  return (
+    <Child color="red" onClick={() => { console.log('clicked')}} >
+      myprop
+    </Child>
+  )
 };
 
 export default Parent;
