5afe1a4b4b55735b34b7b5aee02e0c7f46ec9b0d
Jeanine Schoessler <jeanine.schoessler@khoros.com>
2023-03-16 08:19:48 -0600

    update to react 18 usage

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import Parent from './props/Parent';
 // import UserSearch from './state/UserSearch';
 import EventComponent from './events/EventComponent';
+// import {getLCP, getFID, getCLS} from 'web-vitals';
 
-const App = () => {
-  return (
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <React.StrictMode>
     <div>
       <h1>Partytime</h1>
       {/* <Parent /> */}
       {/* <UserSearch /> */}
       <EventComponent />
     </div>
-  )
-}
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+  </React.StrictMode>
+);
